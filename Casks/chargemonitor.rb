ebd07a61e795b9a286dcc1a5ef7c52ebb28f65bb
crashsystem <crashsystemz@icloud.com>
2026-01-31 21:34:36 +0100

    Add ChargeMonitor cask 1.0.0

---
+++ Casks/chargemonitor.rb
@@ -0,0 +1,12 @@
+cask "chargemonitor" do
+  version "1.0.0"
+  sha256 "08ebfb024d7848f4091753d61812cbf71b03c9ae2c3acb9fbd3f8f4549780e09"
+
+  url "https://github.com/CrashSystemZ/ChargeMonitor/releases/download/v#{version}/ChargeMonitor.dmg",
+      verified: "github.com/CrashSystemZ/ChargeMonitor/"
+  name "ChargeMonitor"
+  desc "Menu bar app to monitor charging status"
+  homepage "https://github.com/CrashSystemZ/ChargeMonitor"
+
+  app "ChargeMonitor.app"
+end
